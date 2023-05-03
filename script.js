7b90e1f5d39467ab023d321f256d36cec07bc8b2
Ektasingh5 <88880749+Ektasingh5@users.noreply.github.com>
2023-05-04 00:31:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // complete the given function
-function isPalindrome(str) {
-  // Convert the string to lowercase and remove non-alphanumeric characters
-  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-  
-  // Check if the string is the same forwards and backwards
-  for (let i = 0; i < str.length / 2; i++) {
-    if (str[i] !== str[str.length - 1 - i]) {
-      return false;
-    }
-  }
-  
-  return true;
+function palindrome(str){
+str=str.toLowerCase();
+let i=0;
+let j=str.length-1;
+while(i<j){
+if(str.charAt(i)==str.charAt(j)){
+i++;
+j--;
+}else{
+return false;
+}
+}
+return true;
 }
 module.exports = palindrome
